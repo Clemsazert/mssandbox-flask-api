0722ed55ea18cc7e293c3ec0e5ab80717e21e387
ClementDidom <clement@freshplanet.com>
2020-06-09 01:20:23 -0400

    feature: added cors and healthcheck

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, json
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+
+@app.route('/healthcheck')
 def hello_world():
-    return 'Hello, World!'
-    
+    return app.response_class(
+        response=json.dumps({"status": "up",
+                             "version": "0.0.1",
+                             "environment": "development"}),
+        status=200,
+        mimetype='application/json'
+    )
+
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
